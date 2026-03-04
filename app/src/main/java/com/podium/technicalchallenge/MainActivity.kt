bb971fd25c36eeb8d7b5b81c3a424a029b4b7819
Brett Tucker <brett.tucker@podium.com>
2026-03-04 14:37:56 -0700

    Enable Hilt for MainActivity

--- app/src/main/java/com/podium/technicalchallenge/MainActivity.kt
+++ app/src/main/java/com/podium/technicalchallenge/MainActivity.kt
@@ -3,7 +3,9 @@ package com.podium.technicalchallenge
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.podium.technicalchallenge.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
