f719577d1652874367b11e25bfe3aa01275f499d
Brett Tucker <brett.tucker@podium.com>
2026-03-04 14:11:38 -0700

    Integrate Hilt for network and data layers

---
+++ app/src/main/java/com/podium/technicalchallenge/common/MovieRepository.kt
@@ -0,0 +1,29 @@
+package com.podium.technicalchallenge.common
+
+import com.google.gson.Gson
+import com.podium.technicalchallenge.entity.MovieEntity
+import com.podium.technicalchallenge.entity.MovieResponse
+import com.podium.technicalchallenge.common.network.queries.Queries
+import com.podium.technicalchallenge.common.network.retrofit.GraphQLService
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import org.json.JSONObject
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class MovieRepository @Inject constructor(private val gson: Gson,
+                                          private val graphQLService: GraphQLService) {
+
+    suspend fun getMovies(): List<MovieEntity> {
+        return withContext(Dispatchers.IO){
+            val paramObject = JSONObject()
+            paramObject.put("query", Queries.getMoviesQuery())
+
+            val response = graphQLService.query(paramObject.toString())
+            val jsonBody = response.body()
+            val data = gson.fromJson(jsonBody, MovieResponse::class.java)
+            data.data.movies
+        }
+    }
+}
