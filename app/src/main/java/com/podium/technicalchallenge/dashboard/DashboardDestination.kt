e1329f1e8a046572e39e7089d616a3856bd1fb9e
Brett Tucker <brett.tucker@podium.com>
2026-03-05 13:02:51 -0700

    @Keep destination

--- app/src/main/java/com/podium/technicalchallenge/dashboard/DashboardDestination.kt
+++ app/src/main/java/com/podium/technicalchallenge/dashboard/DashboardDestination.kt
@@ -1,18 +1,21 @@
 package com.podium.technicalchallenge.dashboard
 
+import androidx.annotation.Keep
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 
 
-
+@Keep
 @Composable
 fun DashboardDestination(){
     val viewModel = hiltViewModel<DashboardViewModel>()
+
     DashboardScreen()
 }
 
@@ -22,6 +25,6 @@ fun DashboardScreen(modifier: Modifier = Modifier) {
         modifier = modifier.fillMaxSize(),
         contentAlignment = Alignment.Center
     ) {
-        Text("Podium Challenge")
+        Text("Podium Challenge", color = Color.White)
     }
 }
